34a45c6736e2d7a11c3a4e67426a9e10e89c0b90
HaoSungogogo <haosun2017@u.northwestern.edu>
2017-07-16 18:00:50 -0500

    TicketMaster DB complete

--- Titan/src/rpc/ItemHistory.java
+++ Titan/src/rpc/ItemHistory.java
@@ -1,7 +1,9 @@
 package rpc;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,54 +15,63 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import db.DBConnection;
+import db.DBConnectionFactory;
+import entity.Item;
+
 /**
  * Servlet implementation class ItemHistory
  */
 @WebServlet("/history")
 public class ItemHistory extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ItemHistory() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private DBConnection conn = DBConnectionFactory.getDBConnection();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ItemHistory() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		try {
 			JSONObject input = RpcHelper.readJsonObject(request);
-			if (input.has("user_id") && input.has("visited")) {
-				String userId = (String) input.get("user_id");
-				JSONArray array = (JSONArray) input.get("visited");
-				List<String> visitedEvents = new ArrayList<>();
-				for (int i = 0; i < array.length(); i++) {
-					String eventId = (String) array.get(i);
-					visitedEvents.add(eventId);
-				}
-				// TODO: logic to process visitedEvents
-				RpcHelper.writeJsonObject(response,
-						new JSONObject().put("status", "OK"));
-			} else {
-				RpcHelper.writeJsonObject(response,
-						new JSONObject().put("status", "InvalidParameter"));
+			String userId = input.getString("user_id");
+			JSONArray array = (JSONArray) input.get("favorite");
+
+			List<String> histories = new ArrayList<>();
+			for (int i = 0; i < array.length(); i++) {
+				String itemId = (String) array.get(i);
+				histories.add(itemId);
 			}
+			conn.setFavoriteItems(userId, histories);
+			RpcHelper.writeJsonObject(response, new JSONObject().put("result", "SUCCESS"));
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doDelete(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
+	}
 }
--- Titan/src/rpc/SearchItem.java
+++ Titan/src/rpc/SearchItem.java
@@ -1,8 +1,8 @@
 package rpc;
 
-import java.util.*;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,12 +11,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
+import db.DBConnection;
+import db.DBConnectionFactory;
 import entity.Item;
-import external.ExternalAPI;
-import external.ExternalAPIFactory;
 
 /**
  * Servlet implementation class SearchItem
@@ -24,29 +23,31 @@ import external.ExternalAPIFactory;
 @WebServlet("/search")
 public class SearchItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SearchItem() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private DBConnection conn = DBConnectionFactory.getDBConnection();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public SearchItem() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String userId = request.getParameter("user_id");
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
 		// Term can be empty or null.
 		String term = request.getParameter("term");
-		ExternalAPI externalAPI = ExternalAPIFactory.getExternalAPI();
-		List<Item> items = externalAPI.search(lat, lon, term);
+		List<Item> items = conn.searchItems(userId, lat, lon, term);
 		List<JSONObject> list = new ArrayList<>();
 		try {
 			for (Item item : items) {
-				// Add a thin version of item object
 				JSONObject obj = item.toJSONObject();
 				list.add(obj);
 			}
@@ -55,13 +56,14 @@ public class SearchItem extends HttpServlet {
 		}
 		JSONArray array = new JSONArray(list);
 		RpcHelper.writeJsonArray(response, array);
-
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
